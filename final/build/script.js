049e1a9cdfb995879f209cab35266139b544caf9
mi-chelle-le <ml3725@drexel.edu>
2023-12-04 14:12:53 -0500

    Final

---
+++ final/build/script.js
@@ -0,0 +1,68 @@
+const productButton = document.querySelector(".product-button");
+const body = document.querySelector(".product-title");
+const carouselContainer = document.querySelector(".carousel-container");
+const leftSlider = document.querySelector(".carousel-prev");
+const rightSlider = document.querySelector(".carousel-next");
+const slider = document.querySelector(".slider");
+const indicatorParent = document.querySelector('.carousel-indicator')
+
+var sectionIndex = 0;
+
+carouselContainer.addEventListener("mouseover", fadeInButtons, false);
+carouselContainer.addEventListener("mouseout", fadeInButtons, false);
+
+rightSlider.addEventListener("click", nextButton);
+leftSlider.addEventListener("click", prevButton);
+productButton.addEventListener("mouseover", underlineText, false);
+productButton.addEventListener("mouseout", underlineText, false);
+
+function nextButton() {
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
+    document.querySelector('.selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+}
+
+function prevButton() {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
+    document.querySelector('.selected').classList.remove('selected');
+    indicatorParent.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+}
+
+
+function underlineText() {
+    body.classList.toggle("underlined-product");
+}
+
+function fadeInButtons() {
+    leftSlider.classList.toggle("carousel-hover");
+    rightSlider.classList.toggle("carousel-hover");
+    indicatorParent.classList.toggle('carousel-hover');
+}
+
+// Save button
+
+// document.querySelector(".save-button").addEventListener('click', function() {
+//     this.classList.toggle('heart-active');
+// });
+
+document.querySelector(".save-button").addEventListener('click', function() {
+    var heartIcon = document.getElementById('heart-icon');
+
+    // Toggle the active class
+    this.classList.toggle('heart-active');
+    if (this.classList.contains('heart-active')){
+        this.setAttribute('data-tooltip', 'Unsave');
+    }
+    else {
+        this.setAttribute('data-tooltip', 'Save');
+    }
+
+
+    // Add pulsing class and then remove it after animation
+    heartIcon.classList.add('pulsing');
+    heartIcon.addEventListener('animationend', function() {
+        heartIcon.classList.remove('pulsing');
+    });
+});
